f8dc935f0a935c439dea8ea73384710fb517aa8d
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-04 17:43:26 +0200

    Added service for sending emails via SendGrid

---
+++ services/sendMail.js
@@ -0,0 +1,22 @@
+const sgMail = require('@sendgrid/mail');
+const { EMAIL_SENDER_ADDRESS } = process.env;
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+async function sendMail({ recieverEmail, topic, messageText, messageMarkup }) {
+  const msg = {
+    to: recieverEmail, // Change to your recipient
+    from: EMAIL_SENDER_ADDRESS, // Change to your verified sender
+    subject: topic,
+    text: messageText,
+    html: messageMarkup,
+  };
+
+  try {
+    await sgMail.send(msg);
+  } catch (error) {
+    console.log(`Error while sending email. \nError message: ${error.message}`);
+  }
+}
+
+module.exports = sendMail;
